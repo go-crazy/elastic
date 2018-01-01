b0b838f85fa6a795188de188fe3de9d0ee3b7fdb
Qylin <18612116114@163.com>
2018-01-01 11:24:06 +0800

    init

---
+++ index.go
@@ -0,0 +1,25 @@
+package Elastic
+
+import (
+	Gin "github.com/gin-gonic/gin"
+	"github.com/go-crazy/elastic/Routers"
+)
+
+func InitElastic(router *Gin.RouterGroup) {
+
+		router.POST("/set", Routers.SetHandler)
+
+		// router.POST("/get", func(c *Gin.Context)  {
+		// 	Routers.GetHandler(c.Writer, c.Request)
+		// })
+		router.POST("/get", Routers.GetHandler)
+		// http://blog.csdn.net/xsdxs/article/details/72849796
+	    router.POST("/map", Routers.MappingHandler)
+}
+
+func TestElastic()  {
+	engine := Gin.Default()
+	wsElastic := engine.Group("test-elastic")
+	InitElastic(wsElastic)
+	engine.Run(":8081")
+}
